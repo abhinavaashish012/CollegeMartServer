3313fb204808851f0d66b6b7ea327f74a3157e51
Abhinav Aashish <abhinavaashish012@gmail.com>
2022-02-26 13:38:56 +0530

    Init Commit

---
+++ src/test/java/com/collegeMart/collegeMart/CollegeMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeMart.collegeMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
